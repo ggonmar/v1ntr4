014abe067d2aa46e206123fa5fe06e3a87ccf2a1
Guillermo Gonzalez <ggonmar@gmail.com>
2021-01-31 20:11:30 +0100

    First implementation of tests in regards to authentication

--- test/auth/README.md
+++ test/auth/README.md
@@ -1,33 +1,63 @@
-## Authentication Test Set
+## Authentication Test Plan
 
-##### AUTH_01 As a user, I want to be able to authenticate myself against the API
-
----
-
-**Given** a user provides valid authentication
-
-**When** a POST request for auth/login is sent
-
-**Then** a 200 response is received`
-
-**And** "access_token" and "token_type" are provided
+### AUTH_01
+#### As a user, I want to be able to authenticate myself against the API
 
 ---
 
-**Given** a user provides invalid authentication
-
-**When** a POST request for auth/login is sent
-
-**Then** a 422 response is received`
-
-**And** a validation error is returned
+**Given** a user provides valid authentication  
+**When** a POST request for auth/login is sent  
+**Then** a 200 response is received  
+&nbsp;&nbsp;**And** "access_token" and "token_type" are provided
 
 ---
 
-**Given** a user provides missing authentication
+**Given** a user provides invalid authentication  
+**When** a POST request for auth/login is sent  
+**Then** a 422 response is received  
+&nbsp;&nbsp;**And** a validation error is returned
 
-**When** a POST request for auth/login is sent
+---
 
-**Then** a 422 response is received`
+**Given** a user provides missing authentication  
+**When** a POST request for auth/login is sent  
+**Then** a 422 response is received  
+&nbsp;&nbsp;**And** a validation error is returned  
 
-**And** a validation error is returned
+---
+---
+
+### AUTH_02
+#### As a previously logged-in user, I want to be able to check my authentication credentials against /auth/me endpoint, so that I can check if I am properly logged in the API.
+
+---
+
+**Given** a user is correctly logged in  
+**When** a GET request is sent against auth/me  
+**Then** a 200 response is received  
+&nbsp;&nbsp;**And** the correct response header is returned
+
+---
+
+**Given** a user is not correctly logged in   
+**When** a GET request is sent against auth/me is  
+**Then** a 401 response is received  
+&nbsp;&nbsp;**And** `detail: Non authenticated` is returned. 
+
+---
+---
+
+### AUTH_03
+#### As a previously logged-in user, I want to be able to logout from the API using /auth/logout endpoint, so that I can no longer use the API.
+
+---
+
+**Given** a user is correctly logged in  
+**When** a GET request is sent against auth/logout  
+**Then** a 200 response (success) is received
+
+--- 
+
+**Given** a user is not correctly logged in  
+**When** a GET request is sent against auth/logout  
+**Then** a 401 response (not authorised) is received
--- test/auth/auth.js
+++ test/auth/auth.js
@@ -1,49 +1,102 @@
 const chai = require(`chai`);
-const request = require('supertest')
-const SERVER_URL ="http://127.0.0.1:8000";
-const TEST_USER = {username:"test", password:"1234"};
+const request = require("supertest");
+const SERVER_URL = "http://127.0.0.1:8000";
+const TEST_USER = { username: "test", password: "1234" };
 
 const should = chai.should();
 
-describe('AUTH_01: The user can authenticate to the system ',()=>{
-    it('Should check the status of the service', async () => {
-        let res = await request(SERVER_URL)
-            .get('/health')
-            .send();
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.body).to.be.deep.equal({
-            "pid": 8,
-            "rss_bytes": 32026624,
-            "num_connections": 2,
-            "cpu_percent": "0.2",
-            "db_status_ok": true,
-            "num_users": 1
-        });
+describe("AUTH_01: The user can authenticate to the system", () => {
+  it("Should check the status of the service", async () => {
+    let res = await request(SERVER_URL).get("/health").send();
+    chai.expect(res.status).to.equal(200);
+    chai.expect(res.body).to.be.deep.equal({
+      pid: 8,
+      rss_bytes: 32026624,
+      num_connections: 2,
+      cpu_percent: "0.2",
+      db_status_ok: true,
+      num_users: 1,
     });
-    it('Should login successfully with valid credentials', async () => {
-        let res = await request(SERVER_URL)
-            .post('/auth/login')
-            .set("content-type", "application/x-www-form-urlencoded")
-            .send(TEST_USER);
-        res.status.should.be.equals(200);
-        res.body.should.haveOwnProperty("access_token");
-        res.body.should.haveOwnProperty("token_type");
-    });
+  });
+  it("Should login successfully with valid credentials", async () => {
+    let res = await request(SERVER_URL)
+      .post("/auth/login")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(TEST_USER);
+    res.status.should.be.equals(200);
+    res.body.should.haveOwnProperty("access_token");
+    res.body.should.haveOwnProperty("token_type");
+  });
 
-    it('Should reject login request with invalid credentials', async () => {
-        let res = await request(SERVER_URL)
-            .post('/auth/login')
-            .set("content-type", "application/x-www-form-urlencoded")
-            .send( {username:"test", password:"1235"});
-        res.status.should.be.equals(422);
-    });
+  it("Should reject login request with invalid credentials", async () => {
+    let res = await request(SERVER_URL)
+      .post("/auth/login")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send({ username: "test", password: "1235" });
+    res.status.should.be.equals(422);
+  });
 
-    it('Should reject login request with missing credentials', async () => {
-        let res = await request(SERVER_URL)
-            .post('/auth/login')
-            .set("content-type", "application/x-www-form-urlencoded")
-            .send( {password:"1234"});
-        res.status.should.be.equals(422);
-        res.body.detail[0].loc.should.be.length(2);
+  it("Should reject login request with missing credentials", async () => {
+    let res = await request(SERVER_URL)
+      .post("/auth/login")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send({ password: "1234" });
+    res.status.should.be.equals(422);
+    res.body.detail[0].loc.should.be.length(2);
+  });
+});
+
+describe("AUTH_02: The user can validate if they are authenticated", () => {
+  it("Should notify a non authenticated user", async () => {
+    let res = await request(SERVER_URL)
+      .get("/auth/me")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send();
+    res.status.should.be.equals(401);
+    res.body.detail.should.be.equals("Not authenticated");
+  });
+  it("Should notify an authenticated user", async () => {
+    let authentication = await request(SERVER_URL)
+      .post("/auth/login")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(TEST_USER);
+
+    let res = await request(SERVER_URL)
+      .get("/auth/me")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .set("Authorization", `Bearer ${authentication.body.access_token}`)
+      .send();
+    res.status.should.be.equals(200);
+    res.body.should.be.deep.equals({
+      id: 1,
+      username: "test",
+      full_name: "User test",
+      email: "test@example.org",
+      disabled: false,
     });
+  });
+});
+
+describe("AUTH_03: The user can logout", () => {
+  it("Should notify a non authenticated user", async () => {
+    let res = await request(SERVER_URL)
+      .get("/auth/logout")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send();
+    res.status.should.be.equals(401);
+    res.body.detail.should.be.equals("Not authenticated");
+  });
+  it("Should logout an authenticated user", async () => {
+    let authentication = await request(SERVER_URL)
+      .post("/auth/login")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(TEST_USER);
+
+    let res = await request(SERVER_URL)
+      .get("/auth/logout")
+      .set("content-type", "application/x-www-form-urlencoded")
+      .set("Authorization", `Bearer ${authentication.body.access_token}`)
+      .send();
+    res.status.should.be.equals(200);
+  });
 });
